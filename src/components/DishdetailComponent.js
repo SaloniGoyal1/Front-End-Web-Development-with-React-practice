e2dad4369bb401273d1d0c5b682ddf816a3a544a
Saloni Goyal <46787992+SaloniGoyal1@users.noreply.github.com>
2020-06-24 15:20:20 +0530

    About Us page Assignment 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,37 +2,40 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-
+// function RenderComments(props){}
 function RenderComments({comments}) {
-        if (comments == null) {
-            return (<div></div>)
-        }
-        const cmnts = comments.map(comment => {
+    if (comments != null) {
+        const cmnts = comments.map((commnts) => {
             return (
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author},
-                    &nbsp;
-                    {new Intl.DateTimeFormat('en-US', {
-                            year: 'numeric',
-                            month: 'long',
-                            day: '2-digit'
-                        }).format(new Date(comment.date))}
-                    </p>
-                </li>
-            )
-        })
-        return (
-            <div className='col-12 col-md-5 m-1'>
-                <h4> Comments </h4>
-                <ul className='list-unstyled'>
-                    {cmnts}
+                <ul key={commnts.id} className="list-unstyled">
+                    <li>
+                        <p> {commnts.comment} </p>
+                        <p> -- {commnts.author},
+                            &nbsp;
+                            {new Intl.DateTimeFormat('en-US', {
+                                year: 'numeric',
+                                month: 'short',
+                                day: '2-digit'
+                            }).format(new Date(Date.parse(commnts.date)))}
+                        </p>
+                    </li>
                 </ul>
-
+            );
+        });
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <h4> Comments </h4>
+                {cmnts}
             </div>
-        )
-}
-    
+        );  
+    } 
+    else {
+        return (
+            <div></div>
+        );
+    }
+}    
+
 function RenderDish({dish}) {
         if (dish != null) {
             return (
@@ -70,26 +73,6 @@ const Dishdetail = (props) => {
                     <div className="row">
                         <RenderDish dish={props.dish} />
                         <RenderComments comments={props.comments} />
-                    </div>
-                </div>
-            )
-        }
-        else if (props.dish) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <h3>{props.dish.name}</h3>
-                            <hr />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderDish dish={props.dish} />
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderComments comments={props.comments} postComment={props.postComment} dishId={props.dish.id} />
-                        </div>
                     </div>
                 </div>
             );
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -15,28 +15,35 @@ import { Link } from 'react-router-dom';
         );
     }
 
-    const Menu = (props) => {
 
+    // function Menu (props) {}
+    const Menu = (props) => {
+        // map (JS keyword): iterating over every dish in the dishes array here
         const menu = props.dishes.map((dish) => {
             return (
-                <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />
-                </div>
+                // key: every item require key attribute to specift it.
+                // the key helps React to recognise each one of these elements, uniquely. 
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <RenderMenuItem dish={dish} />
+                </div>    
             );
-        });
+        }); // take the JS objects
 
-        return (
+        console.log('Menu component render invoked')
+
+         // return what needs to diaplay on the UI in this conponent. (return view)
+        return ( 
             <div className="container">
                 <div className="row">
                     <Breadcrumb>
-                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem> <Link to='home'>Home</Link> </BreadcrumbItem>
                         <BreadcrumbItem active>Menu</BreadcrumbItem>
                     </Breadcrumb>
                     <div className="col-12">
                         <h3>Menu</h3>
                         <hr />
-                    </div>                
-                </div>
+                    </div>    
+                </div>    
                 <div className="row">
                     {menu}
                 </div>
